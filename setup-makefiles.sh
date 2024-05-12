6fd14c0a0728c3f4257a326224416c2ba6df0dca
HatsuneNeeko <160128853+HatsuneNekko@users.noreply.github.com>
2024-05-12 18:33:28 +0800

    Update setup-makefiles.sh

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -11,5 +11,7 @@ set -e
 export DEVICE=cmi
 export DEVICE_COMMON=sm8250-common
 export VENDOR=xiaomi
+export MIKU_GAPPS=true
+export TARGET_WITH_KERNEL_SU=true
 
 "./../../${VENDOR}/${DEVICE_COMMON}/setup-makefiles.sh" "$@"
