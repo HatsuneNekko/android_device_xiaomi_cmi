9a8aef0bf687e4d65dd3b638a8a99c7bfd264d9b
HatsuneNeeko <160128853+HatsuneNekko@users.noreply.github.com>
2024-05-12 18:32:00 +0800

    Update miku_cmi.mk

--- miku_cmi.mk
+++ miku_cmi.mk
@@ -9,7 +9,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common miku stuff.
-$(call inherit-product, vendor/miku/config/common_full_phone.mk)
+$(call inherit-product, vendor/miku/build/product/miku_product.mk)
 
 # Inherit from cmi device
 $(call inherit-product, device/xiaomi/cmi/device.mk)
@@ -28,12 +28,12 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 BUILD_FINGERPRINT := Xiaomi/cmi_global/cmi:13/RKQ1.211001.001/V14.0.5.0.TJAMIXM:user/release-keys
 
 
-# miku
-MIKU_MAINTAINER := Hatsune__Miku_
-TARGET_BOOT_ANIMATION_RES := 1080
-TARGET_GAPPS_ARCH := arm64
-TARGET_SUPPORTS_BLUR := true
-TARGET_SUPPORTS_QUICK_TAP := true
-TARGET_USE_PIXEL_CHARGER := true
-TARGET_FACE_UNLOCK_SUPPORTED := true
-WITH_GAPPS := true
+## Device identifier. This must come after all inclusions
+PRODUCT_NAME := miku_cmi
+PRODUCT_DEVICE := cmi
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := Xiaomi 10 Pro/cmi
+PRODUCT_MANUFACTURER := Xiaomi
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=cmi
